7d8d6053914674b355c445becfb2424198b07954
ianagez <ianagez88@gmail.com>
2018-06-07 13:43:17 +0300

    login is ready

--- www/js/app.js
+++ www/js/app.js
@@ -15,48 +15,18 @@ var mainView = app.views.create('.view-main', {
   domCache:true
 });
 
-
-//$$('.login-screen .list-button').on('click', function () {
-//  var uname = $$('.login-screen input[name = "username"]').val();
-//  var pwd = $$('.login-screen input[name = "password"]').val();
-//  console.log('!!!!!!! Clicked');
-//  mainView.router.load({pageName: 'about'});
-//  //app.alert('Username: ' + uname + ', Password: ' + pwd, function () {
-//  //  app.closeModal('.login-screen');
-//  //});
-//});
-
 //Login Screen Demo
-//$$('#my-login-screen .login-button').on('click', function () {
-//  var username = $$('#my-login-screen [name="username"]').val();
-//  var password = $$('#my-login-screen [name="password"]').val();
-//  if(!(username=="user" && password=='123') ){
-//    // Alert username and password
-//    //app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
-//    app.dialog.alert("Wrong password or login");
-//  }else{
-//    console.log('!!!!! Login wright');
-//    //app.views.create('#main_content-view');
-//   // mainView.router.loadPage({url:'./main_content.html', ignoreCache:true, reload:true });
-//
-//    //var tableView = app.views.create('#main_content_view', {
-//    //  url: '/main_content.html'
-//    //});
-//
-//     //mainView.route({name:'main_content_view'});
-//
-//
-//  }
-//  // Close login screen
-//  //app.loginScreen.close('#my-login-screen');
-//  //$$('#my-login-screen .login-button').path='/main_content/';
-//
-//
-//  //var contentView = app.views.create('.main_content', {
-//  //  url: './pages/main_content.html'
-//  //});
-//
-//});
+$$('#my-login-screen .login-button').on('click', function () {
+  var username = $$('#my-login-screen [name="username"]').val();
+  var password = $$('#my-login-screen [name="password"]').val();
+  if(!(username=="user" && password=='123') ){
+    app.dialog.alert("Неверное имя или пароль","", function(){
+      $$('#my-login-screen [name="username"]').val("");
+      $$('#my-login-screen [name="password"]').val("");
+    });
+  }else
+    mainView.router.navigate('/main_content/');
+});
 
 
 var rowNum=1;
